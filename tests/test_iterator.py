4cb25fdc97fc9fcc8c31f4e29c51c6fc088a890c
lihao57 <lihao57@baidu.com>
2024-03-14 14:03:48 +0800

    [enhance] add unittest

--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -1,65 +1,108 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    :   test_iterator.py
+@Time    :   2024/03/14 13:29:51
+@Author  :   lihao57
+@Version :   1.0
+@Contact :   lihao57@baidu.com
+"""
+
+
+import unittest
 import asyncio
-from src.lh_tool.Iterator import (
+import sys
+
+sys.path.append("../src")
+from lh_tool.iterator import (
     SingleProcess,
     MultiProcess,
+    MultiThread,
     AsyncProcess,
     AsyncMultiProcess,
     ParallelProcess,
 )
 
 
-def process(a, b, opt="+"):
-    if opt == "+":
-        return a + b
-    else:
-        return a - b
+class TestIterator(unittest.TestCase):
+    """TestIterator"""
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-def process_batch(a_list, b_list, opt="+"):
-    res_list = []
-    for a, b in zip(a_list, b_list):
+        self.a = [i for i in range(10)]
+        self.b = [i for i in range(10)]
+        self.res = [i + j for i, j in zip(self.a, self.b)]
+
+    @staticmethod
+    def process(a, b, opt="+"):
+        """process"""
         if opt == "+":
-            res = a + b
+            return a + b
         else:
-            res = a - b
-        res_list.append(res)
-    return res_list
+            return a - b
 
+    @staticmethod
+    def process_batch(a_list, b_list, opt="+"):
+        """process batch"""
+        res_list = []
+        for a, b in zip(a_list, b_list):
+            if opt == "+":
+                res = a + b
+            else:
+                res = a - b
+            res_list.append(res)
+        return res_list
 
-async def async_process(a, b, opt="+"):
-    await asyncio.sleep(1)
-    if opt == "+":
-        return a + b
-    else:
-        return a - b
+    @staticmethod
+    async def async_process(a, b, opt="+"):
+        """async process"""
+        await asyncio.sleep(1)
+        if opt == "+":
+            return a + b
+        else:
+            return a - b
 
+    def test_single_process(self):
+        """test single process"""
+        result_list = SingleProcess(self.process).run(self.a, self.b, opt="+")
+        self.assertEqual(result_list, self.res)
 
-def test_iterator():
-    print("Test lh_tool.MultiProcess")
-    a = [i for i in range(10)]
-    b = [i for i in range(10)]
-    result_list = SingleProcess(process).run(a, b, opt="+")
-    print(result_list)
+    def test_multi_process(self):
+        """test multi process"""
+        result_list = MultiProcess(self.process, nprocs=10).run(
+            self.a, self.b, opt="+"
+        )
+        self.assertEqual(result_list, self.res)
 
-    result_list = MultiProcess(process, nprocs=10).run(a=a, b=b, opt="+")
-    print(result_list)
+    def test_multi_thread(self):
+        """test multi thread"""
+        result_list = MultiThread(self.process, nworkers=2).run(
+            self.a, self.b, opt="+"
+        )
+        self.assertEqual(result_list, self.res)
 
-    result_list = AsyncProcess(async_process).run(a=a, b=b, opt="+")
-    print(result_list)
+    def test_async_process(self):
+        """test async process"""
+        result_list = AsyncProcess(self.async_process).run(
+            self.a, self.b, opt="+"
+        )
+        self.assertEqual(result_list, self.res)
 
-    result_list = AsyncMultiProcess(async_process, nprocs=10).run(
-        a=a, b=b, opt="+"
-    )
-    print(result_list)
+    def test_async_multi_process(self):
+        """test async multi process"""
+        result_list = AsyncMultiProcess(self.async_process, nprocs=10).run(
+            self.a, self.b, opt="+"
+        )
+        self.assertEqual(result_list, self.res)
 
-    ret_list = ParallelProcess(process_batch, nprocs=5).run(
-        a_list=a, b_list=b, opt="+"
-    )
-    result_list = []
-    for ret in ret_list:
-        result_list.extend(ret)
-    print(result_list)
+    def test_parallel_process(self):
+        """test parallel process"""
+        ret_list = ParallelProcess(self.process_batch).run(
+            self.a, self.b, opt="+"
+        )
+        result_list = [_ for ret in ret_list for _ in ret]
+        self.assertEqual(result_list, self.res)
 
 
 if __name__ == "__main__":
-    test_iterator()
+    unittest.main()
