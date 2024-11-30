6268760579f6c998a0cfd9b0e1e796376cc93188
lihao57 <lihao57@baidu.com>
2024-11-30 16:52:40 +0800

    [enhance] pdf2image supports multiple pdf files

--- src/lh_tool/pdf2image.py
+++ src/lh_tool/pdf2image.py
@@ -1,52 +1,85 @@
 import os
+import glob
 import fitz
 import argparse
+from lh_tool.iterator import SingleProcess, MultiProcess
 
 
-def pdf2image(pdf_file, image_file, image_path, zoom=1):
+def pdf2image(pdf_file, image_file, zoom=1):
     pdf = fitz.open(pdf_file)
     trans = fitz.Matrix(zoom, zoom)
-
-    save_to_folder = pdf.page_count > 1 or image_file is None
-    if save_to_folder:
-        os.makedirs(image_path, exist_ok=True)
 
     for index in range(pdf.page_count):
         page = pdf[index]
         pm = page.get_pixmap(matrix=trans)
-        if save_to_folder:
-            image_file = os.path.join(image_path, f"{index + 1:06d}.png")
-        pm.save(image_file)
+        if pdf.page_count > 1:
+            save_file = image_file.replace(".png", f"-{index + 1:06d}.png")
+        else:
+            save_file = image_file
+        pm.save(save_file)
     pdf.close()
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input", type=str, help="input pdf file", required=True
+        "-i", "--input", type=str, help="input pdf file or path", required=True
     )
     parser.add_argument(
-        "-o", "--output", type=str, help="output image file or output path"
+        "-o", "--output", type=str, help="output image file or path"
     )
     parser.add_argument(
         "-z", "--zoom", type=float, default=1, help="zoom image"
     )
+    parser.add_argument(
+        "-r",
+        "--recursive",
+        action="store_true",
+        help="convert pdf to image files recursively",
+    )
+    parser.add_argument(
+        "-n", "--nprocs", type=int, default=1, help="number of process"
+    )
     opts = parser.parse_args()
     print(opts)
 
-    pdf_file = opts.input
-    zoom = opts.zoom
-    if opts.output is None:
-        image_file = os.path.splitext(pdf_file)[0] + ".png"
-    else:
-        postfix = os.path.splitext(opts.output)[1]
-        if len(postfix):
-            image_file = opts.output
+    try:
+        input_path = opts.input
+        output_path = opts.output
+        zoom = opts.zoom
+        recursive = opts.recursive
+        nprocs = opts.nprocs
+        if os.path.isfile(input_path):
+            nprocs = 1
+            input_file_list = [input_path]
+            if output_path is not None:
+                output_file_list = [output_path]
+            else:
+                output_file_list = [os.path.splitext(input_path)[0] + ".png"]
+        elif os.path.isdir(input_path):
+            assert recursive, "Please use -r to convert pdf to image files recursively."
+            input_file_list = glob.glob(os.path.join(input_path, "*.pdf"))
+            output_file_list = []
+            for input_file in input_file_list:
+                if output_path is not None:
+                    filename = os.path.basename(input_file)
+                    filename = os.path.splitext(filename)[0] + ".png"
+                    output_file = os.path.join(output_path, filename)
+                else:
+                    output_file = (
+                            os.path.splitext(input_file)[0] + ".png"
+                    )
+                output_file_list.append(output_file)
         else:
-            image_file = opts.output + ".png"
-    image_path = os.path.splitext(image_file)[0]
+            raise ValueError("Input path must be a file or directory.")
 
-    pdf2image(pdf_file, image_file, image_path, zoom)
+        if nprocs > 1:
+            iterator = MultiProcess(pdf2image, nprocs=nprocs)
+        else:
+            iterator = SingleProcess(pdf2image)
+        iterator.run(input_file_list, output_file_list, zoom=zoom)
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
